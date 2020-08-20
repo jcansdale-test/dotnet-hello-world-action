d3d163737a24c13f6c2be2d1fb2e51d449c9a203
jcansdale-test <60072076+jcansdale-test@users.noreply.github.com>
2020-08-20 16:07:27 +0000

    Add project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_hello_world_action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
